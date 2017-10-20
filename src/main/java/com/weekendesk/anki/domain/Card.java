5c6ca63925842890c1aa0c54ab568db0a636ac26
MLaurenzo <laurent.bertholle@gmail.com>
2017-10-20 23:49:25 +0200

    Add the Card class.

---
+++ src/main/java/com/weekendesk/anki/domain/Card.java
@@ -0,0 +1,19 @@
+package com.weekendesk.anki.domain;
+
+/**
+ * This class represents a card containing a question and its answer.
+ */
+public class Card {
+
+    public final String question;
+    public final String answer;
+
+    public Card(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String toString() {
+        return question + "|" + answer;
+    }
+}
---
+++ src/test/java/com/weekendesk/anki/domain/CardTest.java
@@ -0,0 +1,25 @@
+package com.weekendesk.anki.domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CardTest {
+
+    private static final String question = "MyQuestion";
+    private static final String answer = "MyAnswer";
+
+    @Test
+    public void test_mapping_between_constructor_and_card_fields() {
+        Card card = new Card(question, answer);
+
+        Assert.assertEquals(question, card.question);
+        Assert.assertEquals(answer, card.answer);
+    }
+
+    @Test
+    public void test_toString_method() {
+        Card card = new Card(question, answer);
+
+        Assert.assertEquals("MyQuestion|MyAnswer", card.toString());
+    }
+}
