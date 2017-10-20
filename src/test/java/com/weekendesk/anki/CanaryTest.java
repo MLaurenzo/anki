38652cbea3c6fadc1910e7194833b4c1ed9fa91a
MLaurenzo <laurent.bertholle@gmail.com>
2017-10-20 20:29:19 +0200

    Add canary test to test travis.

---
+++ src/test/java/com/weekendesk/anki/CanaryTest.java
@@ -0,0 +1,11 @@
+package com.weekendesk.anki;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CanaryTest {
+    @Test
+    public void canaryTest() {
+        Assert.assertTrue(false);
+    }
+}
